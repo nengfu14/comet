5b6782a7084eb9bd3dda1cbd19f505b93d788194
Goh Neng Fu <nengfu14@gmail.com>
2025-08-06 16:22:58 +0800

    initial commit

---
+++ wp-content/plugins/advanced-product-labels-for-woocommerce/woocommerce-advanced-products-labels.php
@@ -0,0 +1,19 @@
+<?php
+ /**
+ * Plugin Name: WooCommerce Advanced Product Labels
+ * Plugin URI: https://wordpress.org/plugins/advanced-product-labels-for-woocommerce/
+ * Description: Promote your products! Show “Free Shipping” or other special attributes with your products.
+ * Version: 1.2.9.2
+ * Author: BeRocket
+ * Requires at least: 5.0
+ * Tested up to: 6.8
+ * Requires PHP: 7.0
+ * Requires Plugins: woocommerce
+ * Author URI: https://berocket.com?utm_source=free_plugin&utm_medium=plugins&utm_campaign=products_label
+ * Text Domain: BeRocket_products_label_domain
+ * Domain Path: /languages/
+ * WC tested up to: 10.0
+ */
+define( "BeRocket_products_label_version", '1.2.9.2' );
+define( "BeRocket_products_label_file", __FILE__ );
+include_once('main.php');
